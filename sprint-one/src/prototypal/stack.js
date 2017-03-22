2d1dbb0f55f64d7e4b15ff3bb29690012178be5a
MKS-Student <mks.student@hackreactor.com>
2017-03-22 16:08:47 -0700

    Pass all tests for stack: prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,21 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.storage = {};
+  return stack;
 };
 
 var stackMethods = {};
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+stackMethods.push = function(value) {
+  this.storage[this.size()] = value;
+};
+
+stackMethods.pop = function() {
+  var value = this.storage[this.size() -1];
+  delete this.storage[this.size() -1];
+  return value;
+};
 
