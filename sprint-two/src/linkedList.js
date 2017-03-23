2c5d6d1bc24e87156296242c97f12f3bce318393
MKS-Student <mks.student@hackreactor.com>
2017-03-23 16:06:10 -0700

    Pass all tests for linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,31 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if(list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function() {
+    var headValue = list.head.value;
+    list.head = list.head.next;
+    return headValue;
   };
 
   list.contains = function(target) {
+    var start = list.head;
+    while (start !== null) {
+      if (start.value === target) {
+        return true;
+      } else {
+        start = start.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -25,5 +44,5 @@ var Node = function(value) {
 };
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: addToTail = O(1), removeHead = O(1), contains = O(n)
  */
