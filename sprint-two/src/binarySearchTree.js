37c5a2f8d0850aee54a58c0691e5091efc1b1acc
MKS-Student <mks.student@hackreactor.com>
2017-03-24 19:48:14 -0700

    Pass all tests for binarySearchTree.js

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,61 @@
 var BinarySearchTree = function(value) {
+
+  var bst = Object.create(BinarySearchTree.prototype);
+
+  bst.left = null;
+  bst.right = null;
+  bst.value = value;
+
+  return bst;
+
 };
+
+BinarySearchTree.prototype.insert = function(value) {
+  if (value > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(value);
+    } else {
+    this.right.insert(value);
+    }
+  }
+  if (value < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(value);
+    } else {
+    this.left.insert(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  } else if (value > this.value) {
+    if (this.right === null) {
+      return false;
+    } else {
+      return this.right.contains(value);
+    }
+  } else {
+    if (this.left === null) {
+      return false;
+    } else {
+      return this.left.contains(value);
+    }
+  }
+}
+
+BinarySearchTree.prototype.depthFirstLog = function(callback) {
+  callback(this.value);
+  if (this.right !== null) {
+    this.right.depthFirstLog(callback);
+  }
+  if (this.left !== null) {
+    this.left.depthFirstLog(callback);
+  }
+}
 
 
 /*
- * Complexity: What is the time complexity of the above functions?
+ * Complexity: insert O(log n), contains O(log n), depthFirstLog O(n)
  */
